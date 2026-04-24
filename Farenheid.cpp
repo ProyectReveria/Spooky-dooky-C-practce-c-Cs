a5e63f051b8799f3f36dbd89c928ceaafabace39
Rosary Orchestra Repository (sometimes) <Brokenlevenheart@gmail.com>
2026-04-24 07:14:38 -0600

    Update Farenheid.cpp

--- Farenheid.cpp
+++ Farenheid.cpp
@@ -20,23 +20,28 @@ int main(){
     std:: cin >> unit; 
     unit = std::tolower(unit);
 
-    if (temp <= 0){
-        std::cout << "Invalid Number (only major to 0)" <<std::endl; 
-            std:: cin >> unit; 
-            unit = std::tolower(unit);
-    }
+
 
     switch(unit){
         case 'f' : 
         std::cout << "Enter the celcious emperature"; 
         std::cin >> temp; 
+        if (temp  <= 0){
+            std::cout << "solo numeros majores a 0";
+            std::cin >> temp; 
+        }
         std::cout << "temperatura: " << Self_Math::Fh(temp) << std::endl;
         break;
 
         case 'c' :
         std::cout<< "ender the fahrenheit emperature";
         std::cin>> temp; 
+                if (temp  <= 0){
+            std::cout << "solo numeros majores a 0";
+            std::cin >> temp; 
+        }
         std::cout << "temperatura: " << Self_Math::cls(temp) << std::endl;
+        
         break;
         default:
         std::cout<< "do what the program tell yo to do you know" << std::endl;
@@ -44,3 +49,4 @@ int main(){
     return 0;
 
 }
+
